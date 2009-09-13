293ab3522d25c0e6da4603b24577b6110d3af1d9
suresh.thummalapenta <suresh.thummalapenta@8a563e1c-9374-11de-a8e9-1d0845776227>
2009-09-13 18:28:23 +0000

    Adding mind map to the paper

---
+++ papers/mrmarri-sthumma-PUTStudy/mindmap/Test Generalization.mm
@@ -0,0 +1,27 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1252864232018" ID="Freemind_Link_550459690" MODIFIED="1252864262538" TEXT="Test Generalization">
+<node CREATED="1252864278113" ID="_" MODIFIED="1252865276448" POSITION="right" TEXT="Case study - compare &quot;writing direct puts and generlizing exiting unit tests&quot;  (Subjects: Components of QuickGraph and Facebook)">
+<node CREATED="1252864803865" ID="Freemind_Link_1947875182" MODIFIED="1252864826121" TEXT="Existing unit tests to PUTs - measure coverage and time "/>
+<node CREATED="1252864833967" ID="Freemind_Link_269799143" MODIFIED="1252864858303" TEXT="Directly writing PUTs"/>
+<node CREATED="1252864864134" ID="Freemind_Link_40282700" MODIFIED="1252864954563" TEXT="In case, unit tests do not exist, what is the alternative solution - write unit tests manually or generate automatically using tools such as Randoop"/>
+</node>
+<node CREATED="1252864328524" ID="Freemind_Link_388817313" MODIFIED="1252864380275" POSITION="right" TEXT="Techniques to assist test generlization and writing additional PUTs to improve coverage">
+<node CREATED="1252865003357" ID="Freemind_Link_212576146" MODIFIED="1252865024925" TEXT="Proposed patterns"/>
+<node CREATED="1252865031538" ID="Freemind_Link_344346956" MODIFIED="1252865042030" TEXT="Factory methods"/>
+<node CREATED="1252865042498" ID="Freemind_Link_479089653" MODIFIED="1252865052509" TEXT="Mock objects"/>
+<node CREATED="1252865055393" ID="Freemind_Link_1923610032" MODIFIED="1252865062908" TEXT="Input space partitioning"/>
+</node>
+<node CREATED="1252864408615" ID="Freemind_Link_1610468557" MODIFIED="1252865285743" POSITION="right" TEXT="Empirical results (Subject: NUnit) - benefits of PUTS">
+<node CREATED="1252864508834" ID="Freemind_Link_1149654569" MODIFIED="1252864515834" TEXT="Coverage"/>
+<node CREATED="1252864516609" ID="Freemind_Link_8649006" MODIFIED="1252864519969" TEXT="Defects"/>
+<node CREATED="1252864521521" ID="Freemind_Link_1696363700" MODIFIED="1252864529682" TEXT="Test code maintenance"/>
+</node>
+<node CREATED="1252864611060" ID="Freemind_Link_1587997111" MODIFIED="1252864704248" POSITION="right" TEXT="Future Work - Given the benefits shown with our empirical studies ">
+<node CREATED="1252864643786" ID="Freemind_Link_969991546" MODIFIED="1252864698089" TEXT="R1: To automatically generalize unit tests "/>
+<node CREATED="1252864711326" ID="Freemind_Link_229045513" MODIFIED="1252864724823" TEXT="R2: Sequence generalization"/>
+<node CREATED="1252864713006" ID="Freemind_Link_1167062077" MODIFIED="1252864743359" TEXT="R3: Automatically generation of parameterized mock objects"/>
+<node CREATED="1252864714518" ID="Freemind_Link_1003576579" MODIFIED="1252864795660" TEXT="R4: Feedback guided PUT writing for achieving additional coverage"/>
+</node>
+</node>
+</map>
