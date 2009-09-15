a7770dea2e71015581a39374aae033fd023e636e
suresh.thummalapenta <suresh.thummalapenta@8a563e1c-9374-11de-a8e9-1d0845776227>
2009-09-15 02:25:56 +0000

    Committing the sources

---
+++ papers/mrmarri-sthumma-PUTStudy/mindmap/PUTStudy - Introduction.mm
@@ -0,0 +1,13 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#669900" CREATED="1252980409734" ID="Freemind_Link_830652819" MODIFIED="1252980432872" TEXT="PUTStudy - Introduction">
+<node CREATED="1252980436053" ID="_" MODIFIED="1252980628449" POSITION="right" TEXT="1. Propose a systematic procedure for test generalization"/>
+<node CREATED="1252980437637" ID="Freemind_Link_1594788078" MODIFIED="1252980479742" POSITION="left" TEXT="1. PUTs are more effective than conventional unit tests"/>
+<node CREATED="1252980439101" ID="Freemind_Link_333736957" MODIFIED="1252980643879" POSITION="right" TEXT="2. Empirical evidence showing the benefits of PUTs"/>
+<node CREATED="1252980440829" ID="Freemind_Link_1688895670" MODIFIED="1252980506877" POSITION="left" TEXT="2. Many projects have existing conventional unit tests "/>
+<node CREATED="1252980508521" ID="Freemind_Link_175215158" MODIFIED="1252980663238" POSITION="right" TEXT="3. Empirical evidence showing the Cost vs. Benefit analysis"/>
+<node CREATED="1252980510225" ID="Freemind_Link_1020455947" MODIFIED="1252980542720" POSITION="left" TEXT="3. No systematic procedure or automatic test generalization tools exist"/>
+<node CREATED="1252980554679" ID="Freemind_Link_1506901456" MODIFIED="1252980724158" POSITION="right" TEXT="4. Propose future research direction in automation of test generalization"/>
+<node CREATED="1252980556391" ID="Freemind_Link_295363992" MODIFIED="1252980593814" POSITION="left" TEXT="4. PUTs are supposedly complex to write, what is the Cost vs. Benefit"/>
+</node>
+</map>
