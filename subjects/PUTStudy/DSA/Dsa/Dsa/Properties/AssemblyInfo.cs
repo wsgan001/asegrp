10959643436f0cb92bc42b9235fc712c667d5300
suresh.thummalapenta <suresh.thummalapenta@8a563e1c-9374-11de-a8e9-1d0845776227>
2009-09-20 18:55:21 +0000

    DSA tests

---
+++ subjects/PUTStudy/DSA/Dsa/Dsa/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Data Structures and Algorithms">
+//   Copyright (C) Data Structures and Algorithms Team.
+// </copyright>
+// <summary>
+//   Global assembly settings.
+// </summary>
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dsa")]
+[assembly: AssemblyDescription("Data Structures and Algorithms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dsa")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3ec9c667-f5a4-45f6-b7dd-f0fc8e8dc1f8")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("0.6.*")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: CLSCompliant(true)]
